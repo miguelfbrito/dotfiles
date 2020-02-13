58972f6cf71489d39774ba5bff4fa7f98de6c8c1
Miguel Brito <miguelfbrito11@gmail.com>
2020-02-13 19:06:08 +0000

    Bare repo

---
+++ .config/polybar/launch.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+killall -q polybar
+
+# Wait until the processes have been shut down
+while pgrep -u $UID -x polybar >/dev/null; do sleep 1; done
+
+# polybar -r example &
+
+if type "xrandr"; then
+  for m in $(xrandr --query | grep " connected" | cut -d" " -f1); do
+    MONITOR=$m polybar --reload example &
+  done
+else
+  polybar --reload example &
+fi
+
+echo "Bars launched..."
